be8735f4725cbcab9f0f5579500cc1b88f80dd1f
guojing <343630774@qq.com>
2019-02-19 16:30:01 +0800

    完成 button-group 组件

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('gg-button', Button);
 Vue.component('gg-icon', Icon);
+Vue.component('gg-button-group', ButtonGroup);
 
 new Vue({
     el: "#app",
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="gg-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "button-group"
+    }
+</script>
+
+<style lang="scss">
+    .gg-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        > .gg-button {
+            border-radius: 0;
+            margin-left: -1px;
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+
+            &:last-child {
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover {
+                z-index: 1;
+            }
+        }
+    }
+</style>
