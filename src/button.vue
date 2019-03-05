d680cebd706c4be4c8a8f7077c43a1a7f1aa397a
guojing <343630774@qq.com>
2019-03-05 15:12:41 +0800

    添加 input.vue 及其样式

--- src/button.vue
+++ src/button.vue
@@ -12,6 +12,7 @@
 <script>
     import Icon from './icon'
     export default {
+        name: 'GGButton',
         components: {
             'gg-icon': Icon
         },
@@ -32,7 +33,7 @@
     };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
     @keyframes spin {
         0% {
             transform: rotate(0deg);
--- src/icon.vue
+++ src/icon.vue
@@ -6,10 +6,11 @@
 <script>
     import './svg'
     export default {
+        name: 'GGIcon',
         props: ['name']
     }
 </script>
-<style lang="scss">
+<style lang="scss" scoped>
     .gg-icon {
         width: 1em;
         height: 1em;
