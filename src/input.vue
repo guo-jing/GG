b457cbf4790afe177a76c6b16b83332f1c2c52b8
guojing <343630774@qq.com>
2019-03-07 12:34:16 +0800

    添加 input.vue 的测试用例

--- src/input.vue
+++ src/input.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="wrapper" :class="{error}">
-        <input type="text" :value="value" :disabled="disabled" :readonly="readonly">
+        <input type="text" :value="value" :disabled="disabled" :readonly="readonly"
+               @change="$emit('change', $event)"
+               @input="$emit('input', $event)"
+               @focus="$emit('focus', $event)"
+               @blur="$emit('blur', $event)"
+        >
         <template v-if="error">
             <icon name="error" class="icon-error"></icon>
             <span class="error-message">{{error}}</span>
@@ -9,6 +14,7 @@
 </template>
 <script>
     import Icon from './icon';
+
     export default {
         components: {
             Icon
@@ -38,28 +44,33 @@
     $border-color-hover: #666;
     $border-radius: 4px;
     $font-size: 14px;
-    $box-shadow-color: rgba(0,0,0,0.5);
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
     $red: #f1453d;
     .wrapper {
         font-size: $font-size;
         display: inline-flex;
         align-items: center;
+
         > :not(:last-child) {
             margin-right: .5em;
         }
+
         > input {
             height: $height;
             border: 1px solid $border-color;
             border-radius: $border-radius;
             padding: 0 8px;
             font-size: inherit;
+
             &:hover {
                 border-color: $border-color-hover;
             }
+
             &:focus {
                 box-shadow: inset 0 1px 3px $box-shadow-color;
                 outline: none;
             }
+
             &[disabled],
             &[readonly] {
                 border-color: #bbb;
@@ -67,14 +78,17 @@
                 cursor: not-allowed;
             }
         }
+
         &.error {
             > input {
                 border-color: $red;
             }
         }
+
         .icon-error {
             fill: $red;
         }
+
         .error-message {
             color: $red;
         }
