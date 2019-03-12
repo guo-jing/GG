43cb68ea85ec06a1e9fe3f66bcb3afc5657a2c68
guojing <343630774@qq.com>
2019-03-12 11:49:26 +0800

    完成 row 和 col 组件的雏形

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 import Input from './input'
+import Row from './row'
+import Col from './col'
 
 Vue.component('gg-button', Button);
 Vue.component('gg-icon', Icon);
 Vue.component('gg-button-group', ButtonGroup);
 Vue.component('gg-input', Input);
+Vue.component('gg-row', Row);
+Vue.component('gg-col', Col);
 
 new Vue({
     el: "#app",
@@ -16,10 +20,5 @@ new Vue({
         loading2: true,
         loading3: false,
         message: '王五'
-    },
-    methods: {
-        inputChange(e) {
-            console.log(e.target.value);
-        }
     }
 });
